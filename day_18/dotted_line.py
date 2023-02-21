4ad42b0ab4459255e10ff8c619836ea1fec11841
John Lunz <johnlunz8@gmail.com>
2023-02-21 01:02:35 -0500

    made script to draw dotted line using turtle

---
+++ day_18/dotted_line.py
@@ -0,0 +1,16 @@
+import turtle
+
+
+def draw_dotted_line(turtle, n):
+    turtle.pd()
+    turtle.fd(n)
+    turtle.pu()
+    turtle.fd(n)
+
+
+if __name__ == '__main__':
+    t = turtle.Turtle()
+    for _ in range(15):
+        draw_dotted_line(t, 15)
+    screen = turtle.Screen()
+    screen.exitonclick()
