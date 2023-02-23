bcc48221af370649c834442d21cbf474fc80339c
John Lunz <johnlunz8@gmail.com>
2023-02-22 23:14:34 -0500

    added script that draws diff sided shapes

---
+++ day_18/diff_shapes.py
@@ -0,0 +1,28 @@
+import turtle
+import random
+
+
+def get_random_hex_color():
+    # remove first 2 digits of hex number and add # to make valid color code
+    return '#' + hex(random.randrange(0, 2**24))[2:]
+
+
+def turn_turtle(turtle, angle):
+    turtle.fd(100)
+    turtle.right(angle)
+
+
+def draw_shape(num_sides, turtle):
+    hex_color = get_random_hex_color()
+    turtle.pencolor(hex_color)
+    for _ in range(num_sides):
+        turn_turtle(turtle=turtle, angle=360/n)
+
+
+if __name__ == '__main__':
+    t = turtle.Turtle()
+    # make up to ten sided shapes
+    for n in range(11)[3:]:
+        draw_shape(n, turtle)
+    screen = turtle.Screen()
+    screen.exitonclick()
